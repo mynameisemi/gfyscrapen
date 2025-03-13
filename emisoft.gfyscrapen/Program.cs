426fa84b202d6b729d447641ba1ca8459c5d76fa
d9mz <101074954+d9mz@users.noreply.github.com>
2025-03-13 00:13:28 -0700

    feat: httpserver

---
+++ emisoft.gfyscrapen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+using EmiSoft;
+
+namespace EmiSoft
+{
+    class GfyScrapen
+    {
+	public static void Main(string[] args)
+	{
+	    // flow
+	    // load config.json, read port & key
+	    // load sites/ recurs. httpserver reads
+
+	    // webserver sends request to current http. to prevent hanging,
+	    // return a response, then create new thread w/ selenium & scrape
+	    // then, after results, encode json then send back to webserver w/
+	    // key to prevent hanging
+	    
+	    Console.WriteLine("starting http server");
+
+            HttpServer.start();
+        }
+    }
+}
